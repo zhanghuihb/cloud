5d7d1e00632db0bb2b21beb3e9967856a7ddd3a3
zhanghuihb <zhanghuihb@126.com>
2020-04-15 18:15:32 +0800

    strong

--- user/src/main/java/com/burton/cloud/user/UserApplication.java
+++ user/src/main/java/com/burton/cloud/user/UserApplication.java
@@ -3,7 +3,7 @@ package com.burton.cloud.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.burton.cloud.common.domain.user")
